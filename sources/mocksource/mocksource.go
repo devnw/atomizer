8cda34b493fc3d2ec2193210ddbdf3af031dbddc
Benji Vesterby <benji.vesterby@gmail.com>
2019-02-28 18:11:52 -0700

    Added some comments regarding different features

--- sources/mocksource/mocksource.go
+++ sources/mocksource/mocksource.go
@@ -7,6 +7,12 @@ import (
 )
 
 // TODO: Setup quarks and callbacks
+// TODO: Add timeouts to the payload coming from the message source *time.Duration, if nil then it's ignored and the context uses a standard cancel context
+//  otherwise it uses the timeout context with the duration from the payload
+//  also include other types of payload identifiers like "long running" continuous, etc...
+
+// TODO: setup a sub package that can be added to add logging metrics to the atomizer so that it will show performance of each atom and which atom is running, etc...
+//  possibly could even include the option to turn on the profiler
 
 func GetMockSource(atoms int, delay *time.Duration, process func(ctx context.Context, payload []byte) (err error)) (source mocksource) {
 	return mocksource{
@@ -22,6 +28,9 @@ type mocksource struct {
 	process func(ctx context.Context, payload []byte) (err error)
 }
 
+// TODO: Atoms are not what is returned from a source, the sources return electrons which are what allow
+//  the atom to do it's work this is in the form of a []byte which allows for atomizer to be serialization agnostic since
+//  the deserialization will occur through the implementation of the atom itself rather than in this atomizer library
 func (this mocksource) GetAtoms() <- chan mockatom {
 	var atomStream = make(chan mockatom)
 
@@ -44,6 +53,10 @@ func (this mocksource) GetAtoms() <- chan mockatom {
 	}(atomStream)
 
 	return atomStream
+}
+
+type mockelectron struct {
+
 }
 
 type mockatom struct {
